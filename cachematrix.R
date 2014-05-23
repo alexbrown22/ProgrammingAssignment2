8f036fedceb7651a406d01e50208d79f70d08a48
alexbrown22 <alexbrown22@users.noreply.github.com>
2014-05-23 21:58:55 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The makeCacheMatrix() function creates a special "matrix" object that can cache its inverse. It is 
+## essentially a list of instructions to set and retrieve a matrix and its inverse.
+## The function produces objects 'x' and 's' within the parent makeCacheMatrix() environment.
+## setmatrix() creates objects 'x' and 's' in the global environment (free variables)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        setmatrix <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        getmatrix <- function() x
+        setinverse <- function(solve) s <<- solve
+        getinverse <- function() s
+        list(setmatrix = setmatrix, getmatrix = getmatrix,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
+## The cacheSolve() function returns a matrix that is the inverse of 'x' as defined by makeCacheMatrix.
+## If the inverse has not yet been computed (i.e. s = NULL), it will compute it and return it
+## as the result. 
+## If the inverse has already been computed (i.e. s is not NULL) then it will retrieve it from 
+## the cache without computing it again. 
+## I have added the two 'print' commands to let the user know if, upon running cacheSolve, they are 
+## retrieving cached data or calculating new data.  
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getinverse()
+        if(!is.null(s)) {
+                print("getting cached data")
+                return(s)
+        }
+        data <- x$getmatrix()
+        s <- solve(data, ...)
+        x$setinverse(s)
+        print("calculating new data")
+        s    
 }
